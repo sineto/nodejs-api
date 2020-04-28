579d13ddb89a6cfbdc73fdb5e9a1c4de4acbe8b7
Sinésio Neto <sn.logbackup@gmail.com>
2020-04-28 10:04:01 -0300

    feature: Add pagination with mongoose-paginate

--- src/controllers/ProductController.js
+++ src/controllers/ProductController.js
@@ -4,8 +4,9 @@ const Product = mongoose.model('Product');
 
 module.exports =  {
   async index(request, response) {
-    const products = await Product.find();
-    return response.json(products);
+		const { page = 1 } = request.query;
+		const products = await Product.paginate({}, { page: page, limit: 5 });
+  	return response.json(products);
   },
 
   async show(request, response) {
