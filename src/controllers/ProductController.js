dfc098e1e1b8b4c2791515404a93e0c058c90f50
Sinésio Neto <sn.logbackup@gmail.com>
2020-04-28 08:50:45 -0300

    feature: Add SHOW route and method

--- src/controllers/ProductController.js
+++ src/controllers/ProductController.js
@@ -8,6 +8,11 @@ module.exports =  {
     return response.json(products);
   },
 
+  async show(request, response) {
+    const product = await Product.findById(request.params.id);
+    return response.json(product);
+  },
+
   async create(request, response) {
     const products = await Product.create(request.body);
     return response.json(products);
