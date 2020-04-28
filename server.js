6aa4e851233cda93095ab1ffae67b7071e2ef49c
Sinésio Neto <sn.logbackup@gmail.com>
2020-04-28 10:09:10 -0300

    update: Add cors to application

--- server.js
+++ server.js
@@ -1,10 +1,16 @@
 const express = require('express');
+const cors = require('cors');
 const mongoose = require('mongoose');
 const requireDir = require('require-dir');
 
 // Iniciando App
 const app = express();
+
+// permite express retornar requisições recebidas como JSON
 app.use(express.json());
+
+// dentro do método cors é possível colocar os dominios que serão restritoa à essa aplicação
+app.use(cors());
 
 // Conectando com o DB
 mongoose.connect('mongodb://localhost:27017/producthunt', {
